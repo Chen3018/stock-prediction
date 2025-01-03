c2b66074e55a7446e4e468cce98cae99c7db52d7
Chen3018 <chenjiafan3018@gmail.com>
2025-01-02 21:44:16 -0500

    Update MainLayout.jsx

--- frontend/stock-1.0.0/src/layouts/MainLayout.jsx
+++ frontend/stock-1.0.0/src/layouts/MainLayout.jsx
@@ -3,12 +3,12 @@ import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import NavBar from '../components/NavBar';
+import Navbar from '../components/Navbar';
 
 const MainLayout = () => {
   return (
     <>
-        <NavBar />
+        <Navbar />
         <Outlet />
         <ToastContainer />
     </>
