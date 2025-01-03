3babe80ed0a89c0210a6c6428d5703e3d2384d52
Chen3018 <chenjiafan3018@gmail.com>
2025-01-03 01:48:37 -0500

    test constant

--- backend/middleware/allowCORs.js
+++ backend/middleware/allowCORs.js
@@ -2,7 +2,7 @@ const setCORsHeaders = (req, res, next) => {
     const origin = req.headers.origin;
     console.log(origin);
     console.log(process.env.FRONTEND_URL);
-    if (origin.contains(process.env.FRONTEND_URL)) {
+    if (origin.contains('stock-prediction')) {
         console.log('Setting CORs headers');
         res.setHeader('Access-Control-Allow-Origin', origin);
         res.setHeader('Access-Control-Allow-Methods', 'GET');
